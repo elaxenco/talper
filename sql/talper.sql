00d8a2973fe929b30a5526e29b5c856449b422dc
elaxenco <40673618+elaxenco@users.noreply.github.com>
2019-03-08 18:20:49 -0700

    base de datos

---
+++ sql/talper.sql
@@ -0,0 +1,55 @@
+/*
+SQLyog Ultimate v9.63 
+MySQL - 5.6.28-log : Database - microfinanciera
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`microfinanciera` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `microfinanciera`;
+
+/*Table structure for table `departamentos` */
+
+DROP TABLE IF EXISTS `departamentos`;
+
+CREATE TABLE `departamentos` (
+  `Puesto` int(11) NOT NULL AUTO_INCREMENT,
+  `Descripcion` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`Puesto`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `departamentos` */
+
+insert  into `departamentos`(`Puesto`,`Descripcion`) values (1,'Sistemas'),(2,'Mercadotecnia'),(3,'Credito'),(4,'Contabilidad');
+
+/*Table structure for table `empleados` */
+
+DROP TABLE IF EXISTS `empleados`;
+
+CREATE TABLE `empleados` (
+  `Clave_Emp` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `ApPaterno` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `ApMaterno` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `FecNac` date NOT NULL,
+  `Departamento` int(11) NOT NULL,
+  `Sueldo` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`Clave_Emp`),
+  KEY `Departamento` (`Departamento`),
+  CONSTRAINT `empleados_ibfk_1` FOREIGN KEY (`Departamento`) REFERENCES `departamentos` (`Puesto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `empleados` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
